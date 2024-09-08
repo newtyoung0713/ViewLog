7da3ca0334eefb0498972fb6fb8f47f7b95d47df
Yang <e2223845@gmail.com>
2024-09-08 18:27:10 -0400

    v0.12 one more source for records

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
       { source: '/api/register', destination: 'http://localhost:5000/register' },
       { source: '/api/login', destination: 'http://localhost:5000/login' },
       { source: '/api/addRecord', destination: 'http://localhost:5000/addRecord' },
+      { source: '/api/records', destination: 'http://localhost:5000/records' },
     ];
   },  
 };
