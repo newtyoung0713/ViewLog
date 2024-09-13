5f49d571f89bbfd24738efd3c50108d07fd4b9d3
Yang <e2223845@gmail.com>
2024-09-13 12:19:44 -0400

    v0.15 organized the routes

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,10 +4,18 @@ const nextConfig = {
 
   async rewrites() {
     return [
+      // User registration and login routes
       { source: '/api/register', destination: 'http://localhost:5000/register' },
       { source: '/api/login', destination: 'http://localhost:5000/login' },
-      { source: '/api/addRecord', destination: 'http://localhost:5000/addRecord' },
+      
+      // For creating a new record (POST request)
+      { source: '/api/records/new', destination: 'http://localhost:5000/newRecord' },
+      
+      // For getting all records (GET request)
       { source: '/api/records', destination: 'http://localhost:5000/records' },
+
+      // For handling a specific record by ID (GET, PUT, DELETE requests)
+      { source: '/api/records/:id', destination: 'http://localhost:5000/records/:id' }
     ];
   },  
 };
